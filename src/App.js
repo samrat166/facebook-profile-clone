f568a39b6add322100acafbfd7bf8daea6015e8d
samrat <samratojha16@gmail.com>
2023-01-07 23:44:51 +0545

    -post added

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Profile from "./Profile";
+
+function App() {
+  return <Profile />;
+}
+
+export default App;
